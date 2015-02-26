ebfaa485a3194c3e639c42493a2a760b49e8e580
Alison Tague <alisonmtague@gmail.com>
2015-02-25 19:38:14 -0500

    broke my wishlist linking to its items but delete kind of works ;(

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,21 +1,19 @@
  class WishlistsController < ApplicationController
 	def index
-
-		# if current_user == Wishlist.find(params[user_id])
-		# 	Wishlist.user_id
-		# end
-
-
-
 		@wishlists = Wishlist.where(user_id: current_user.id)
-		# @current_user = User.find(params[:id])
-		# @wishlists = Wishlist.find(params[current_user])
-# if current id = wishlist user_id then display wishlist
 	end
 
 	def show
 		@wishlist = Wishlist.find(params[:id])
-
 	end
 
+	def destroy
+		@wishlist = Wishlist.find(params[:id])
+		@wishlist.destroy
+		redirect_to wishlist_path
+	end
+
+	def new
+		@wishlist = Wishlist.new
+	end
 end
